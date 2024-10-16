6832916ecf39c87151ca6579606137806511f88f
LandonTheCoder <100165458+LandonTheCoder@users.noreply.github.com>
2024-10-16 08:40:34 -0400

    Change "static inline" to "inline" I misunderstood what static inline meant, inline implies static unless necessary to have external linkage. It may also make it less likely to inline it.
    This changes its appearances in utils.h but not bitmap.c++.

--- utils.h
+++ utils.h
@@ -9,16 +9,16 @@ const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 // Utility functions
-static inline double degrees_to_radians(double degrees) {
+inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
-static inline double random_double() {
+inline double random_double() {
     // rand() returns an integer between 0 and RAND_MAX
     return std::rand() / (RAND_MAX + 1.0);
 }
 
-static inline double random_double(double min, double max) {
+inline double random_double(double min, double max) {
     // Random real between min, max inclusive
     return min + (max - min) * random_double();
 }
